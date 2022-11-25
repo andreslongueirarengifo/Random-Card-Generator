a5040c6012024cc8daede17b9d6f547837f252d0
Andres Longueira <andreslongueirarengifo@gmail.com>
2022-11-25 19:14:03 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const numbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const figures = ["♦", "♥", "♠", "♣"];
+  const colors = ["red", "black"];
+  const figure1 = document.querySelector(".figure1");
+  const number = document.querySelector(".number");
+  const figure2 = document.querySelector(".figure2");
+  const btnChange = document.querySelector("#change-card");
+
+  const randomFigure = figures[Math.floor(Math.random() * 4)];
+  const randomColor = colors[Math.floor(Math.random() * 2)];
+  const changeColor = () => {
+    figure1.innerHTML = randomFigure;
+    number.innerHTML = numbers[Math.floor(Math.random() * 13)];
+    figure2.innerHTML = randomFigure;
+
+    figure1.style.color = randomColor;
+    figure2.style.color = randomColor;
+  };
+
+  btnChange.addEventListener("click", changeColor);
 };
